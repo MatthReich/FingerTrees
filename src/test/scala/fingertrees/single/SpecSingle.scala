df83b0180c097902b9cd4c39c09bef5f1553f681
Matthias Reichenbach <matthreich@gmail.com>
2022-11-22 17:18:03 +0100

    added single test

--- src/test/scala/fingertrees/single/SpecSingle.scala
+++ src/test/scala/fingertrees/single/SpecSingle.scala
@@ -1,2 +1,20 @@
 package fingertrees.single
 
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import single.implSingle.Single
+
+
+class SpecSingle extends AnyWordSpec with Matchers {
+
+  "A Single" when {
+
+    val single: Single[Int] = Single(10)
+
+    "calling toString" should {
+      "be presented right" in {
+        single.toString should be("Single( 10 )")
+      }
+    }
+  }
+}
