456b79990c5104af9c4e59e7eb9cb7dc5194fd62
Matthias Reichenbach <matthreich@gmail.com>
2022-11-23 12:40:48 +0100

    added open type for head

--- src/main/scala/fingertree/implFingerTree/FingerTree.scala
+++ src/main/scala/fingertree/implFingerTree/FingerTree.scala
@@ -4,9 +4,18 @@ import fingertree.IFingerTree
 import node.INode
 import empty.IEmpty
 import empty.implEmpty.Empty
+import single.ISingle
+import single.implSingle.Single
+import deep.IDeep
+import deep.implDeep.Deep
 
-class FingerTree[A](treeHead: IEmpty = Empty()) extends IFingerTree[A] {
-  override def append(entry: A): FingerTree[A] = ???
+final case class FingerTree[A](treeHead: IEmpty[A] | ISingle[A] | IDeep[A] = Empty[A]()) extends IFingerTree[A] {
+  override def append(entry: A): FingerTree[A] = 
+    treeHead match 
+      case Empty[A]() => this.copy(treeHead.asInstanceOf[IEmpty[A]].+:(entry))
+      case Single[A](one) => this.copy(treeHead.asInstanceOf[ISingle[A]].+:(entry))
+      case Deep[A](one, two, three) => this.copy(treeHead.asInstanceOf[IDeep[A]].+:(entry))
+      case _ => this.copy() // TODO error handling
 
   override def remove(entry: A): IFingerTree[A] = ??? 
 
@@ -17,6 +26,4 @@ class FingerTree[A](treeHead: IEmpty = Empty()) extends IFingerTree[A] {
   override def head: A = ???
   override def last: A = ???
   override def toString(): String = treeHead.toString
-
-
 }
