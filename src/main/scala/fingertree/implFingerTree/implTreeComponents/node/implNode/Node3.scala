5b38109182e9cf29ddb491b16c07f028bcb6d7ca
Matthias Reichenbach <matthreich@gmail.com>
2022-12-07 14:07:17 +0100

    new folder structure for better overview

--- src/main/scala/fingertree/implFingerTree/implTreeComponents/node/implNode/Node3.scala
+++ src/main/scala/fingertree/implFingerTree/implTreeComponents/node/implNode/Node3.scala
@@ -2,5 +2,5 @@ package node.implNode
 
 import node.INode
 
-final case class Node3[A](one: A | INode[A], two: A | INode[A], three: A | INode[A]) extends INode[A]:
+final case class Node3[A](one: A, two: A, three: A) extends INode[A]:
   override def toString: String = String.format("Node( %s, %s, %s)", one.toString(), two.toString(), three.toString())
