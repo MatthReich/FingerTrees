0886774c659adc7c09eda2632115f4660521bae9
Matthias Reichenbach <matthreich@gmail.com>
2022-12-19 11:39:49 +0100

    appended deep test

--- src/test/scala/fingertrees/deep/SpecDeep.scala
+++ src/test/scala/fingertrees/deep/SpecDeep.scala
@@ -10,12 +10,23 @@ import digit.implDigit.Digit2
 import digit.implDigit.Digit3
 import digit.implDigit.Digit4
 import node.implNode.Node3
+import empty.IEmpty
+import single.ISingle
+import deep.IDeep
+import node.nodeImpl.Node2
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.when
+import view.implView.implViewLeftCons.ViewLeftCons
+import view.implView.implViewRightCons.ViewRightCons
 
 class SpecDeep extends AnyWordSpec with Matchers {
 
   "A Deep" when {
 
-    val deep: Deep[Int] = Deep(Digit1(10), Empty(), Digit1(9))
+    val deep: IDeep[Int] = Deep(Digit1(10), Empty(), Digit1(9))
+    val mockedDigit = mock(classOf[Digit1[Int]])
+    val mockedEmpty = mock(classOf[Empty])
+    val mockedSingle = mock(classOf[Single[Node3[Int]]])
 
     "appending a new element" should {
       "return a Deep with 2 Digits on right when 1 Digit before" in {
@@ -64,15 +75,47 @@ class SpecDeep extends AnyWordSpec with Matchers {
       }
     }
 
+    "concating TreeComponents" should {
+      "return a itself when concating an Empty" in {
+        val emptyToConcat: IEmpty = Empty()
+
+        val concated = deep ++ emptyToConcat
+
+        concated should be(deep)
+      }
+
+      "return a Deep( Digit1 Empty Digit2 ) when concating a Single where the right Digit contains the new entry on last position" in {
+        val singleToConcat: ISingle[Int] = Single[Int](8)
+
+        val concated = deep ++ singleToConcat
+
+        concated should be(Deep(Digit1(10), Empty(), Digit2(9, 8)))
+      }
+
+      "return a Deep( Digit1 Single( Node2 ) Digit1 ) when concating a Deep where Digit2 has as first entry the old singe entry" in {
+        val deepToConcat: IDeep[Int] = Deep(Digit1(8), Empty(), Digit1(7))
+
+        val concated = deep ++ deepToConcat
+
+        concated should be(Deep(Digit1(10), Single(Node2(9, 8)), Digit1(7)))
+      }
+    }
+
     "checking size" should {
       "be 2 when only 2 Digits and 1 Empty" in {
+        when(mockedDigit.size) thenReturn 1
+        when(mockedEmpty.size) thenReturn 0
+        val deep: IDeep[Int] = Deep(mockedDigit, mockedEmpty, mockedDigit)
+
         deep.size should be(2)
       }
 
-      "be 6 when 1 Digit left, 1 Single with Node3 middle and 2 Digits right" in {
-        val newDeep = deep.:+(8).:+(7).:+(6).:+(5)
+      "be 5 when Deep( Digit1 Single( Node3 ) Digit1 )" in {
+        when(mockedDigit.size) thenReturn 1
+        when(mockedSingle.size) thenReturn 3
+        val deep: IDeep[Int] = Deep(mockedDigit, mockedSingle, mockedDigit)
 
-        newDeep.size should be(6)
+        deep.size should be(5)
       }
     }
 
@@ -84,19 +127,58 @@ class SpecDeep extends AnyWordSpec with Matchers {
 
     "accessing head" should {
       "return right head element" in {
+        when(mockedDigit.head) thenReturn Some(10)
+        val deep: IDeep[Int] = Deep(mockedDigit, Empty(), Digit1(9))
+
         deep.head should be(Some(10))
       }
     }
 
     "accessing last" should {
       "return right last element" in {
-        deep.last should be(Some(9))
+        when(mockedDigit.last) thenReturn Some(10)
+        val deep: IDeep[Int] = Deep(Digit1(9), Empty(), mockedDigit)
+
+        deep.last should be(Some(10))
       }
     }
 
     "calling toString" should {
       "be presented right" in {
         deep.toString should be("Deep( Digit( 10 ), Empty(), Digit( 9 ) )")
+      }
+    }
+
+    "viewRight" should {
+      "return the own entry as last and empty as init" in {
+        deep.viewRight should be(Some(ViewRightCons(10, Empty())))
+      }
+    }
+
+    "viewLeft" should {
+      "return None when no head of prefix exists" in {
+        when(mockedDigit.head) thenReturn None
+        val deep: IDeep[Int] = Deep(mockedDigit, Empty(), mockedDigit)
+  
+        deep.viewLeft should be(None)
+      }
+
+      "return head of prefix and tail of suffix when tail is None" in {
+        when(mockedDigit.head) thenReturn Some(10)
+        when(mockedDigit.tail) thenReturn None
+        when(mockedDigit.toTreeComponent) thenReturn Single(9)
+        val deep = Deep(mockedDigit, Empty(), mockedDigit)
+  
+        deep.viewLeft should be(Some(ViewLeftCons(10, Single(9))))
+      }
+
+      "return head of prefix and tail of suffix when tail is Some" in {
+        when(mockedDigit.head) thenReturn Some(10)
+        when(mockedDigit.tail) thenReturn Some(Digit1(10))
+        when(mockedDigit.toTreeComponent) thenReturn Single(9)
+        val deep = Deep(mockedDigit, Empty(), mockedDigit)
+  
+        deep.viewLeft should be(Some(ViewLeftCons(10, Deep(Digit1(10), Empty(), mockedDigit))))
       }
     }
   }
