2f63e5069b71b25de96f3ef0bcd8fb963d31e343
Matthias Reichenbach <matthreich@gmail.com>
2022-12-19 08:22:02 +0100

    added node2 spec

--- src/test/scala/fingertrees/nodes/SpecNode2.scala
+++ src/test/scala/fingertrees/nodes/SpecNode2.scala
@@ -3,16 +3,61 @@ package fingertrees.nodes
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import node.nodeImpl.Node2
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.when
+import empty.implEmpty.Empty
+import digit.implDigit.Digit1
+import single.implSingle.Single
+import deep.implDeep.Deep
+import node.INode
+import empty.IEmpty
+import fingertree.implFingerTree.ITreeComponent
+import deep.IDeep
+import node.implNode.Node3
 
 class SpecNode2 extends AnyWordSpec with Matchers {
 
   "A Node with 2 elements" when {
 
-    val node: Node2[Int] = Node2(10, 9)
+    val node: INode[Int] = Node2(10, 9)
+    val mockedEmpty = mock(classOf[Empty])
+    val mockedDigit = mock(classOf[Digit1[Int]])
+    val mockedSingle = mock(classOf[Single[Int]])
+    val mockedDeep = mock(classOf[Deep[Int]])
+    val mockedNode = mock(classOf[Node3[Int]])
 
-    "checking size" should {
-      "return 2" in {
+     "checking size" should {
+      "be 2 when only values are stored" in {
         node.size should be(2)
+      }
+
+      "be 0 when Emptys are stored" in {
+        when(mockedEmpty.size) thenReturn 0
+        val node: INode[ITreeComponent[Nothing]] = Node2(mockedEmpty, mockedEmpty)
+
+        node.size should be(0)
+      }
+
+      "be 1 when Empty and Single are stored" in {
+        when(mockedEmpty.size) thenReturn 0
+        when(mockedSingle.size) thenReturn 1
+        val node: INode[ITreeComponent[Int]] = Node2(mockedEmpty, mockedSingle)
+
+        node.size should be(1)
+      }
+
+      "be 2 when 2 Deep( Digit1 Empty Digit1 ) are stored" in {
+        when(mockedDeep.size) thenReturn 2
+        val node: INode[ITreeComponent[Int]] = Node2(mockedDeep, mockedDeep)
+
+        node.size should be(4)
+      }
+
+      "be 3 when Node3 is stored" in {
+        when(mockedNode.size) thenReturn 3
+        val node: INode[INode[Int]] = Node2(mockedNode, mockedNode)
+
+        node.size should be(6)
       }
     }
 
