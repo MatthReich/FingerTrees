08fe720cc37c3a2a77eb7e7caf92f3db54f929d9
Matthias Reichenbach <matthreich@gmail.com>
2022-12-26 14:31:18 +0100

    added tests for fingertree

--- src/test/scala/SpecFingerTree.scala
+++ src/test/scala/SpecFingerTree.scala
@@ -1,4 +1,4 @@
-package fingertrees
+package fingertree.implFingerTree
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -6,8 +6,8 @@ import fingertree.implFingerTree.FingerTree
 import fingertree.implFingerTree.ITreeComponent
 import org.scalacheck.Gen
 import org.scalacheck.Arbitrary.arbitrary
-import fingertree.IFingerTree
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import fingertree.IFingerTree
 
 class SpecFingerTree
     extends AnyWordSpec
@@ -55,10 +55,8 @@ class SpecFingerTree
           count += 1
           fingerTree =
             if (Gen.oneOf("prepend", "append").sample.get == "prepend") then
-              println("prepend")
               fingerTree.prepend(toAppend)
             else
-              println("append")
               fingerTree.append(toAppend)
           fingerTree.size should be(count)
         }
@@ -78,11 +76,108 @@ class SpecFingerTree
       }
     }
 
+    "checking if isEmpty" should {
+      "be true when only created" in {
+        fingerTree.isEmpty should be (true)
+      }
+
+      "be false when element inside" in {
+        fingerTree.append(10).isEmpty should be (false)
+      }
+    }
+
+    "getting head" should {
+      "return None when empty" in {
+        fingerTree.head should be (None)
+      }
+
+      "return Some when something is stored" in {
+        fingerTree.append(10).head should be (Some(10))
+      }
+    }
+
+    "getting last" should {
+      "return None when empty" in {
+        fingerTree.last should be (None)
+      }
+
+      "return some when something is stored" in {
+        fingerTree.append(10).last should be (Some(10))
+      }
+    }
+
+    "getting init" should {
+      "be None when nothing is stored" in {
+        fingerTree.init should be (None)
+      }
+    }
+
+    "getting tail" should {
+      "be None when nothing is stored" in {
+        fingerTree.tail should be (None)
+      }
+    }
+
+    "calling toList" should {
+      "return all elements in right order when appending" in {
+        var fingerTree: IFingerTree[Int] = FingerTree()
+        var list: List[Int] = Nil;
+        fingerTree.toList should be (list)
+
+        forAll(ints) { (toAppend: Int) =>
+          list = list :+ toAppend
+          fingerTree = fingerTree.append(toAppend)
+          fingerTree.toList should be (list)
+        }
+      }
+
+      "return all elements in right order when prepending" in {
+        var fingerTree: IFingerTree[Int] = FingerTree()
+        var list: List[Int] = Nil;
+        fingerTree.toList should be (list)
+
+        forAll(ints) { (toAppend: Int) =>
+          list = toAppend +: list
+          fingerTree = fingerTree.prepend(toAppend)
+          fingerTree.toList should be (list)
+        }
+      }
+
+      "gets correctly after mixing append and prepend" in {
+        var fingerTree: IFingerTree[Int] = FingerTree()
+        var list: List[Int] = Nil;
+        fingerTree.toList should be (list)
+
+        forAll(ints) { (toAppend: Int) =>
+          fingerTree =
+            if (Gen.oneOf("prepend", "append").sample.get == "prepend") then
+              list = toAppend +: list
+              fingerTree.prepend(toAppend)
+            else
+              list = list :+ toAppend
+              fingerTree.append(toAppend)
+          fingerTree.toList should be (list)
+        }
+      }
+
+      "gets correctly after every concat" in {
+        val fingerTreeToConcat: IFingerTree[Int] =
+          FingerTree().append(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        var fingerTree: IFingerTree[Int] = FingerTree()
+        var list: List[Int] = Nil;
+
+        forAll { () =>
+          list = list ::: List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+          fingerTree = fingerTree.concat(fingerTreeToConcat)
+          fingerTree.toList should be (list)
+        }
+      }
+    }
+
     "calling toString" should {
       "be presented right when only created" in {
         fingerTree.toString should be("Empty()")
       }
     }
   }
-
 }
